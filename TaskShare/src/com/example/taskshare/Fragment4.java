339f1d90b71f35b6040e76f5d00e33453d2d5d70
linyang625 <lyang@hmc.edu>
2013-11-16 05:21:30 -0800

    fragments in progress

---
+++ TaskShare/src/com/example/taskshare/Fragment4.java
@@ -0,0 +1,19 @@
+package com.example.taskshare;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+ 
+public class Fragment4 extends Fragment {
+ 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+ 
+        View rootView = inflater.inflate(R.layout.fragment_4, container, false);
+         
+        return rootView;
+    }
+}
